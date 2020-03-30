85006c40e7bf61dcf8594e3d31c0082e826281db
Pietro Di Bello <pierodibello@gmail.com>
2020-03-30 15:35:16 +0200

    improve IT

--- src/test/kotlin/happiness/infrastructure/HappinessHandlerTestIT.kt
+++ src/test/kotlin/happiness/infrastructure/HappinessHandlerTestIT.kt
@@ -3,22 +3,24 @@ package happiness.infrastructure
 import io.restassured.RestAssured.post
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.nio.charset.Charset
 
 class HappinessHandlerTestIT {
 
     @Test
     fun `route respond correctly`() {
-        val response = post("$URL/happiness/1")
+        post("$BASE_URL/happiness/1")
             .then()
             .statusCode(201)
             .extract()
-            .body().asByteArray().toString(Charset.defaultCharset())
-
-        assertThat(response).isEqualTo("Thanks for voting :D")
+            .body()
+            .asString() shouldBe "Thanks for voting :D"
     }
 
     companion object {
-        private const val URL = "https://8y4rzbgztl.execute-api.eu-west-1.amazonaws.com/dev"
+        private const val BASE_URL = "https://8y4rzbgztl.execute-api.eu-west-1.amazonaws.com/dev"
     }
+}
+
+private infix fun Any?.shouldBe(value: Any?) {
+    assertThat(this).isEqualTo(value)
 }
