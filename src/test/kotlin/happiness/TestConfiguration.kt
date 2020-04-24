eb5cdf4231d98243a908477c73745f53db02de34
Angelo Ceccato <angelo.ceccato@it.clara.net>
2020-04-24 14:47:43 +0200

    remove getting endpoint from environment variable

--- src/test/kotlin/happiness/TestConfiguration.kt
+++ src/test/kotlin/happiness/TestConfiguration.kt
@@ -1,6 +1,6 @@
 package happiness
 
 val BASE_URL: String by lazy {
-    System.getenv("HAPPINESS_INDEX_BASE_URL") ?: "https://g49lpxwuhd.execute-api.eu-west-1.amazonaws.com/dev"
+    "https://g49lpxwuhd.execute-api.eu-west-1.amazonaws.com/dev"
 }
 
