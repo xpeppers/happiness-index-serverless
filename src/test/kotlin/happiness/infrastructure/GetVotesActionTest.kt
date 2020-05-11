aee1fb6598499214c3e0e8da82fca23eedce6a42
Pietro Di Bello <pierodibello@gmail.com>
2020-05-11 14:21:32 +0200

    test that responds with all the stored votes when asking for all votes

--- src/test/kotlin/happiness/infrastructure/GetVotesActionTest.kt
+++ src/test/kotlin/happiness/infrastructure/GetVotesActionTest.kt
@@ -3,10 +3,12 @@ package happiness.infrastructure
 import daikon.core.Request
 import daikon.core.Response
 import happiness.GetHappinessVotesUseCase
+import happiness.UserVote
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 
 class GetVotesActionTest {
 
@@ -15,12 +17,20 @@ class GetVotesActionTest {
     private val response = mockk<Response>(relaxed = true)
 
     @Test
-    fun `responds with HTTP 200 when asking for votes`() {
-        every { getVotesUseCase.execute() } returns emptyList()
+    fun `responds with all the stored votes when asking for all votes`() {
+        every { getVotesUseCase.execute() } returns listOf(
+            UserVote(
+                vote = 1,
+                date = LocalDateTime.parse("2011-03-02T11:23:00"),
+                location = "any",
+                userId = "any"
+            )
+        )
 
         GetVotesAction(getVotesUseCase).handle(request, response, DummyContext())
 
         verify { response.status(200) }
+        verify { response.write("""{"votes":[{"vote":1,"userId":"any","location":"any","date":"2011-03-02T11:23:00"}]}""") }
     }
 }
 
